c4d74e695474c3c4c8f2321996ad3d0f9ba5aa4c
Violet Stewart <vkazoo2@gmail.com>
2021-09-30 14:00:58 -0700

    added some html

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { add } from '../calculations.js';
+
 
 const test = QUnit.test;
 
@@ -15,4 +17,15 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('add function', (expect) => {
+   //arrange
+    const input1 = 3;
+    const input2 = 2; 
+    const expected = 5;
+   //act
+    const actual = add(input1, input2);
+    //assert
+    expect.equal(actual, expected, 'add test');
 });
