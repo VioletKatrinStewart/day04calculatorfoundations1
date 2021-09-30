59879d6c54a87774cb815883bd2f5141b96d131d
Violet Stewart <vkazoo2@gmail.com>
2021-09-30 14:10:32 -0700

    javascript added for add button

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // import functions and grab DOM elements
+const addInput1 = document.getElementById('add-input-1');
+const addInput2 = document.getElementById('add-input-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
 
+console.log(addInput1, addInput2, addButton, addAnswer);
 // initialize global state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+addButton.addEventListener('click', ()=>{
+    console.log('add button was clicked');
+});
